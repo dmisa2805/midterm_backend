46e96ea8b09a34144820b8e0173d1d4b0e20ee38
dmisa2805 <doanthidieuhang28@gmail.com>
2025-05-19 18:38:58 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 //Register
 export const registerUser = async (req, res) => {
     const { userName, email, password } = req.body;
+    //External Error
     try {
         if (!userName || !email || !password) {
             return res.status(400).json({ error: "All fields must be filled" });
@@ -13,11 +14,15 @@ export const registerUser = async (req, res) => {
         if (existingUser) {
             return res.status(409).json({ error: "Email already exists" });
         }
+        //Hash password
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new userModel({ userName, email, password: hashedPassword });
+        //Success
         await newUser.save();
         res.status(201).json({ message: "User has been successfully created"});
-    } catch (error) {
+    } 
+    //Internal Error
+    catch (error) {
         console.log(error);
         return res.status(500).json({ error: "Internal Server Error" });
     }
@@ -26,7 +31,11 @@ export const registerUser = async (req, res) => {
 //Login
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
+    //External Error
     try {
+    if (!user || !password) {
+        return res.status(400).json({ error: "All fields must be filled" });
+    }
     const user = await userModel.findOne({ email });
     if (!user) {
         return res.status(404).json({ error: "User not found" });
@@ -35,12 +44,16 @@ export const loginUser = async (req, res) => {
     if (!verifyPassword) {
         return res.status(401).json({error:"Invalid input"})
     }
+    //Modify Random String
     const randomString = uuidv4();
     const apiKey = `mern-${user._id}-${email}-${randomString}`;
     user.apiKey = apiKey;
+    //Success
     await user.save();
     res.status(200).json({ message: "User has been successfully logged in", apiKey });
-} catch (err) {
+} 
+//Internal Error
+catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal Server Error" });
 }
